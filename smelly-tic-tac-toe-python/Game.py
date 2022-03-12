a7b7162ea66d4e357cacfe999a5294aaeff4f586
javikalsan <jcalzadosanchez@gmail.com>
2022-03-12 19:56:17 +0100

    refactor: extract method

--- smelly-tic-tac-toe-python/Game.py
+++ smelly-tic-tac-toe-python/Game.py
@@ -2,63 +2,57 @@ from Board import Board
 
 
 class Game(object):
+    ROWS = [0, 1, 2]
+    EMPTY = ' '
+    PLAYER_O = 'O'
 
     def __init__(self):
-        self._lastSymbol = ' '
+        self._lastSymbol = self.EMPTY
         self._board = Board()
 
     def play(self, symbol, x, y):
+        self._apply_validations(symbol, x, y)
+        self._update_game_state(symbol, x, y)
+
+    def winner(self):
+        for row in self.ROWS:
+            if self._is_empty_row(row):
+                continue
+            if self._is_three_in_a_row(row):
+                return self._board.TileAt(row, 0).Symbol
+        return self.EMPTY
+
+    def _apply_validations(self, symbol, x, y):
         if self._is_first_move() and self._is_O_player_turn(symbol):
             raise Exception('Invalid first player')
-
         if self._is_player_repeated(symbol):
             raise Exception('Invalid next player')
-
         if self._is_tile_used(x, y):
             raise Exception('Invalid position')
 
-        # update game state
+    def _update_game_state(self, symbol, x, y):
         self._lastSymbol = symbol
         self._board.AddTileAt(symbol, x, y)
 
     def _is_first_move(self):
-        return self._lastSymbol == ' '
+        return self._lastSymbol == self.EMPTY
 
     def _is_O_player_turn(self, symbol):
-        return symbol == 'O'
+        return symbol == self.PLAYER_O
 
     def _is_player_repeated(self, symbol):
         return symbol == self._lastSymbol
 
     def _is_tile_used(self, x, y):
-        return self._board.TileAt(x, y).Symbol != ' '
+        return self._board.TileAt(x, y).Symbol != self.EMPTY
 
-    def winner(self):
-        # if the positions in first row are taken
-        if self._board.TileAt(0, 0).Symbol != ' ' \
-                and self._board.TileAt(0, 1).Symbol != ' ' \
-                and self._board.TileAt(0, 2).Symbol != ' ':
-            # if first row is full with same symbol
-            if self._board.TileAt(0, 0).Symbol == self._board.TileAt(0, 1).Symbol \
-                    and self._board.TileAt(0, 2).Symbol == self._board.TileAt(0, 1).Symbol:
-                return self._board.TileAt(0, 0).Symbol
+    def _is_three_in_a_row(self, row):
+        return self._board.TileAt(row, 0).Symbol == self._board.TileAt(row, 1).Symbol \
+                and self._board.TileAt(row, 2).Symbol == self._board.TileAt(row, 1).Symbol
 
-        # if the positions in first row are taken
-        if self._board.TileAt(1, 0).Symbol != ' ' \
-                and self._board.TileAt(1, 1).Symbol != ' ' \
-                and self._board.TileAt(1, 2).Symbol != ' ':
-            # if first row is full with same symbol
-            if self._board.TileAt(1, 0).Symbol == self._board.TileAt(1, 1).Symbol \
-                    and self._board.TileAt(1, 2).Symbol == self._board.TileAt(1, 1).Symbol:
-                return self._board.TileAt(1, 0).Symbol
-
-        # if the positions in first row are taken
-        if self._board.TileAt(2, 0).Symbol != ' ' \
-                and self._board.TileAt(2, 1).Symbol != ' ' \
-                and self._board.TileAt(2, 2).Symbol != ' ':
-            # if first row is full with same symbol
-            if self._board.TileAt(2, 0).Symbol == self._board.TileAt(2, 1).Symbol \
-                    and self._board.TileAt(2, 2).Symbol == self._board.TileAt(2, 1).Symbol:
-                return self._board.TileAt(2, 0).Symbol
-
-        return ' '
+    def _is_empty_row(self, row):
+        if self._board.TileAt(row, 0).Symbol == self.EMPTY \
+                and self._board.TileAt(row, 1).Symbol == self.EMPTY \
+                and self._board.TileAt(row, 2).Symbol == self.EMPTY:
+            return True
+        return False
