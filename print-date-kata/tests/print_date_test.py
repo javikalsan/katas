4449e2b7efa59850f76977b02fc752a5cace9afa
javikalsan <jcalzadosanchez@gmail.com>
2022-03-19 21:46:55 +0100

    First iteration

--- print-date-kata/tests/print_date_test.py
+++ print-date-kata/tests/print_date_test.py
@@ -1,11 +1,45 @@
 import unittest
+from unittest.mock import Mock
+
 from print_date import PrintDate, Calendar, Printer
 
-class PrintDateTest(unittest.TestCase):
 
-    def test_xxx(self):
-        print_date = PrintDate(Calendar(), Printer())
+TODAY_VALUE = '2022-03-19'
+
+
+class PrintDateTest(unittest.TestCase):
+    def test_using_unittest_mock(self):
+        calendar = Mock(Calendar)
+        printer = Mock(Printer)
+        calendar.today = Mock(return_value=TODAY_VALUE)
+
+        print_date = PrintDate(calendar, printer)
 
         print_date.print_current_date()
 
-        # I don't know how to test it
+        printer.print_line.assert_called_with(TODAY_VALUE)
+
+    def test_using_own_mocks(self):
+        calendar = CalendarDouble()
+        printer = PrinterDouble()
+        print_date = PrintDate(calendar, printer)
+
+        print_date.print_current_date()
+
+        self.assertTrue(printer.print_line_have_been_called_with(TODAY_VALUE))
+
+
+class CalendarDouble(Calendar):
+    def today(self):
+        return TODAY_VALUE
+
+
+class PrinterDouble(Printer):
+    def __init__(self):
+        self.print_line_have_been_called_value = None
+
+    def print_line(self, line):
+        self.print_line_have_been_called_value = line
+
+    def print_line_have_been_called_with(self, value):
+        return self.print_line_have_been_called_value == value
